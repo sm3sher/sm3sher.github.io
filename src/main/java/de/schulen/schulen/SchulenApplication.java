70ae62844927bd97b4695b2b94707bb1952cc563
Aljosha <almei105@hhu.de>
2018-11-12 13:40:48 +0100

    Added real project

---
+++ src/main/java/de/schulen/schulen/SchulenApplication.java
@@ -0,0 +1,12 @@
+package de.schulen.schulen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchulenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchulenApplication.class, args);
+	}
+}
